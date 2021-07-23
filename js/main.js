a2a6389353e8df7f658e8e09b72ec6321ba654a4
Kunal Ghosh <kunalghosh.res@gmail.com>
2021-07-23 08:49:14 +0530

    fixed scrolling and navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(".nav-link").click(function () {
+  var href = $(this).attr('href');
+  $(".navbar-nav .nav-link").removeClass('active');
+  $(this).addClass('active');
+  $('html,body').animate({
+      scrollTop: $(href).offset().top + -100
+    },
+    'fast');
+});
